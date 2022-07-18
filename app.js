f9fec555fafff0b9edffbbaf4eb5a6ce06ebab93
Vera <veraopoku100@gmail.com>
2022-07-17 22:31:24 -0400

    es6/Debugging

---
+++ app.js
@@ -0,0 +1,62 @@
+var y = 7
+var z = 13
+
+function addNumber(y, z) {
+    return y+z;
+}
+
+    console.log(addNumber(7,13))
+
+
+function subtractNumber(z, y) {
+    return z-y;
+}
+
+    console.log(subtractNumber(13,7))
+
+function multiplyNumber(y, z) {
+    return y*z;
+    }
+    
+    console.log(multiplyNumber(13,7))
+
+    
+function divideNumber(y, z) {
+    return y/z;
+    }
+    
+    console.log(divideNumber(7,13))
+
+    
+// Arrow Function
+
+const sum = (firstParam, secondParam) => {
+    return firstParam + secondParam;
+};
+
+    console.log(sum(7,13));
+
+
+    const difference = (firstParam, secondParam) => {
+    return firstParam - secondParam;
+};
+
+    console.log(difference(13,7));
+
+
+    const multiply = (firstParam, secondParam) => {
+    return firstParam * secondParam;
+};
+
+    console.log(multiply(7,13));
+
+
+    const divide = (firstParam, secondParam) => {
+    return firstParam / secondParam;
+};
+
+    console.log(divide(7,13));
+
+    
+
+
